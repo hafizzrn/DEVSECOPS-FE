44e0b1d303c530abc44d75cd1d91faab74b3f7ce
kenziehh <ghaisani.nurani@gmail.com>
2025-10-17 16:46:08 +0700

    feat(auth): add logout functionality and update header with conditional rendering for login/logout buttons

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -2,7 +2,8 @@
 
 import logo from "@/assets/logo.png";
 import { Button } from "@/components/ui/button";
-import { CreditCard, LayoutDashboard, User } from "lucide-react";
+import { CreditCard, LayoutDashboard, LogOut, User } from "lucide-react";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
@@ -10,6 +11,7 @@ import { usePathname, useRouter } from "next/navigation";
 export function Header() {
   const router = useRouter();
   const pathname = usePathname();
+  const session = useSession();
 
   return (
     <header className="bg-white border-border">
@@ -57,6 +59,25 @@ export function Header() {
               <User className="h-4 w-4" />
               Profile
             </Button>
+            {
+              session.data?.user?.access_token ?
+                <Button
+                  onClick={() => router.push("/auth/logout")}
+                  variant={pathname === "/auth/logout" ? "default" : "ghost"}
+                  size="default"
+                  className={`rounded-full gap-2 px-6 py-2 text-sm font-medium ${pathname === "/auth/logout" ? "bg-foreground text-background hover:bg-foreground/90" : "text-foreground/80 hover:bg-muted"}`}
+                >
+                  <LogOut className="h-4 w-4" />
+                  Logout
+                </Button> :
+                <Button
+                  onClick={() => router.push("/auth/login")}
+                  variant={"default"}
+                  size="default"
+                >
+                  Login
+                </Button>
+            }
           </nav>
         </div>
       </div>
